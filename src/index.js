e9e28e7db5f387b08f62e7d16a5f33b3185b95d3
Alexstraszza <aostapenko66@gmail.com>
2018-02-22 18:18:10 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -5,23 +5,18 @@ class Sorter {
             return a - b
         }
     }
-
     add(element) {
         this.array.push(element);
     }
-
     at(index) {
         return this.array[index];
     }
-
     get length() {
         return this.array.length;
     }
-
     toArray() {
         return this.array;
     }
-
     sort(indices) {
         let tmp = []
         for (let i = 0; i < indices.length; i++) {
@@ -32,13 +27,9 @@ class Sorter {
         for (let i = 0; i < indices.length; i++) {
             this.array[indices[i]] = tmp[i]
         }
-
     }
-
     setComparator(compareFunction) {
         this.comparator = compareFunction
     }
 }
-
-
 module.exports = Sorter
